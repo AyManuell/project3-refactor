cdb6ac5b6653044e5ce9260995dd85afa8009c33
AyManuell <nuelayo97@gmail.com>
2022-11-20 00:27:02 +0100

    updated commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=postgresadmin
+export POSTGRES_PASSWORD=vnp-1234
+export POSTGRES_HOST=db-aymanuell-project3.c1kpafe96gxy.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project3db
+export AWS_BUCKET=arn:aws:s3:::project3-2057-1096-733-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=WeWilldef3ndyou
 export URL=http://localhost:8100
